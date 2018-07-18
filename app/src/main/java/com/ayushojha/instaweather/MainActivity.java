e06f19e9515f7d116344f0e4581e9dd292a34ce1
code-vault <devarshi.new@gmail.com>
2018-07-19 04:14:50 +0530

    added location feature

--- app/src/main/java/com/ayushojha/instaweather/MainActivity.java
+++ app/src/main/java/com/ayushojha/instaweather/MainActivity.java
@@ -1,41 +1,145 @@
 package com.ayushojha.instaweather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ayushojha.instaweather.gsonclasses.OpenWeatherJSONResponse;
 import com.ayushojha.instaweather.util.OpenWeatherAPIHandler;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
-import java.text.DateFormat;
+import com.squareup.picasso.Picasso;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class MainActivity extends AppCompatActivity{
-    TextView textCity, textTime, textIcon, textTemp, textWeatherDesc,textHumidity, textWindSpeed, textPressure;
-    double lat = 26.8177, lon =82.7633;
+public class MainActivity extends AppCompatActivity {
+    TextView textCity, textTime, textIcon, textTemp, textWeatherDesc, textHumidity, textWindSpeed, textPressure;
+    ImageView imgIcon;
+    double lat, lon;
+    Typeface weatherFont;
+
+    FusedLocationProviderClient client;
+    static final int MY_FINE_LOCATION_REQUEST = 101;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getSupportActionBar().hide();
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        weatherFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/weathericons-regular-webfont.ttf");
+
         textCity = findViewById(R.id.textCity);
         textTime = findViewById(R.id.textTime);
-        textIcon = findViewById(R.id.textIcon);
+//        textIcon = findViewById(R.id.textIcon);
         textTemp = findViewById(R.id.textTemp);
         textWeatherDesc = findViewById(R.id.textWeatherDesc);
         textWindSpeed = findViewById(R.id.textWindSpeed);
         textPressure = findViewById(R.id.textPresure);
         textHumidity = findViewById(R.id.textHumidity);
-        new GetWeather().execute(OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
-        Log.d("APIRequest", OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
+//        textIcon = findViewById(R.id.textIcon);
+//        textIcon.setTypeface(weatherFont);
+        imgIcon = findViewById(R.id.imgIcon);
 
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(7500); //should use 15000
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        lat = location.getLatitude();
+                        lon = location.getLongitude();
+                        new GetWeather().execute(OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
+                        Log.d("APIRequest", OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
+                    }
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_FINE_LOCATION_REQUEST);
+        }
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                for (Location location : locationResult.getLocations()) {
+                    //update the user interface with user data
+                    if (location != null) {
+                        lat = location.getLatitude();
+                        lon = location.getLongitude();
+                        new GetWeather().execute(OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
+                        Log.d("APIRequest", OpenWeatherAPIHandler.getAPIRequest(String.valueOf(lat), String.valueOf(lon)));
+                    }
+                }
+            }
+        };
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_FINE_LOCATION_REQUEST:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission granted
+                } else {
+                    Toast.makeText(getApplicationContext(), "This app needs location permisson to work.", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        client.removeLocationUpdates(locationCallback);
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            client.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_FINE_LOCATION_REQUEST);
+        }
     }
 
     public class GetWeather extends AsyncTask<String, Void, String> {
@@ -50,7 +154,7 @@ public class MainActivity extends AppCompatActivity{
 
         @Override
         protected void onPostExecute(String s) {
-            DateFormat df = new SimpleDateFormat();
+            DateFormat df = new SimpleDateFormat("h:mm a z");
 
             try {
                 super.onPostExecute(s);
@@ -60,8 +164,13 @@ public class MainActivity extends AppCompatActivity{
                 //txtLastUpdate.setText(String.format("Last Updated: %s", Common.getDateNow()));
                 textWeatherDesc.setText(String.format("%s",response.getWeather().get(0).getDescription()));
                 textHumidity.setText(String.format("%d%%",response.getMain().getHumidity()));
-                textTime.setText(String.format("%s/%s",df.format(new Date((int)response.getDt()))));
+                textTime.setText(df.format(response.getDt()*1000));
                 textTemp.setText(String.format("%.2f °C",response.getMain().getTemp()));
+                textWindSpeed.setText(String.format("%.2f km/h",response.getWind().getSpeed()*18/5));
+                textPressure.setText(String.format("%.2f hPa", response.getMain().getPressure()));
+                //textIcon.setText(Html.fromHtml(OpenWeatherAPIHandler.setWeatherIcon(response.getWeather().get(0).getId(), (long) response.getSys().getSunrise() *1000, (long) response.getSys().getSunset()*1000)));
+
+                Picasso.get().load(OpenWeatherAPIHandler.setWeatherIcon(response.getWeather().get(0).getIcon())).into(imgIcon);
             } catch (Exception e) {
                 e.printStackTrace();
             }
--- app/src/main/java/com/ayushojha/instaweather/util/OpenWeatherAPIHandler.java
+++ app/src/main/java/com/ayushojha/instaweather/util/OpenWeatherAPIHandler.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -45,6 +46,40 @@ public class OpenWeatherAPIHandler {
         }
         return data;
     }
+
+    public static String setWeatherIcon(String iconId) {
+        return String.format("http://openweathermap.org/img/w/%s.png",iconId);
+    }
+
+    public static String setWeatherIcon(int actualId, long sunrise, long sunset){
+        int id = actualId / 100;
+        String icon = "";
+        if(actualId == 800){
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+                icon = "&#xf00d;";
+            } else {
+                icon = "&#xf02e;";
+            }
+        } else {
+            switch(id) {
+                case 2 : icon = "&#xf01e;";
+                    break;
+                case 3 : icon = "&#xf01c;";
+                    break;
+                case 7 : icon = "&#xf014;";
+                    break;
+                case 8 : icon = "&#xf013;";
+                    break;
+                case 6 : icon = "&#xf01b;";
+                    break;
+                case 5 : icon = "&#xf019;";
+                    break;
+            }
+        }
+        return icon;
+    }
+
 
 
 }
